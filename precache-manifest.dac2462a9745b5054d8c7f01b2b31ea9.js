dc96708f224b67c5ddc11b7030e8e4228cd3afd6
5377 <pradipamate07@gmail.com>
2020-05-28 01:18:19 +0530

    Updates

---
+++ precache-manifest.dac2462a9745b5054d8c7f01b2b31ea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f54306320851d7c83cd",
+    "url": "/songlist/static/css/main.8655e75e.chunk.css"
+  },
+  {
+    "revision": "3f54306320851d7c83cd",
+    "url": "/songlist/static/js/main.b56036cd.chunk.js"
+  },
+  {
+    "revision": "1acab3123af7fd4bab5d",
+    "url": "/songlist/static/js/runtime~main.0b96cd11.js"
+  },
+  {
+    "revision": "6581cfbcabf799c7a81c",
+    "url": "/songlist/static/css/2.e4623dd3.chunk.css"
+  },
+  {
+    "revision": "6581cfbcabf799c7a81c",
+    "url": "/songlist/static/js/2.96b35849.chunk.js"
+  },
+  {
+    "revision": "f06b60879aff18f3b9d420117c69340f",
+    "url": "/songlist/index.html"
+  }
+];
